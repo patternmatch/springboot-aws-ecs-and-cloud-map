4e26857b3bce84f885d727fd1b508999aee1c06c
Sebastian Feduniak <sebastian.feduniak@pattern-match.com>
2020-07-14 12:03:41 +0200

    Textprocessor: adding stub endpoints

---
+++ src/textprocessor/src/main/java/com/patternmatch/ecs/textprocessor/model/TextInfo.java
@@ -0,0 +1,8 @@
+package com.patternmatch.ecs.textprocessor.model;
+
+import lombok.Value;
+
+@Value
+public class TextInfo {
+    private String language;
+}
--- src/textprocessor/src/main/java/com/patternmatch/ecs/textprocessor/resource/TextProcessor.java
+++ src/textprocessor/src/main/java/com/patternmatch/ecs/textprocessor/resource/TextProcessor.java
@@ -1,5 +1,26 @@
 package com.patternmatch.ecs.textprocessor.resource;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+import com.patternmatch.ecs.textprocessor.model.TextInfo;
+import com.patternmatch.ecs.textprocessor.model.TextInput;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
+
+@Slf4j
+@RestController
+@RequestMapping(path = "/textprocessor", produces = APPLICATION_JSON_VALUE)
 public class TextProcessor {
 
+    @PostMapping
+    public TextInfo info(@RequestBody TextInput input) {
+        log.info("Received input: [{}]", input.getText());
+
+        return new TextInfo(Locale.getISOLanguages()[(int) (System.currentTimeMillis() % 10)]);
+    }
 }
